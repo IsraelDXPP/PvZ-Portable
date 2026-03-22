e82efbefe65ae4b1dcb17392c4285e7795703446
IsraelDXPP <nydilupi75@hotmail.com>
2026-03-21 21:18:51 -0700

    Switch breakpoint?

--- src/SexyAppFramework/platform/switch/Window.cpp
+++ src/SexyAppFramework/platform/switch/Window.cpp
@@ -36,92 +36,98 @@ using namespace Sexy;
 
 void SexyAppBase::MakeWindow()
 {
-	// Connect to the EGL default display
-	mWindow = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-	if (!mWindow)
-		return;
+    // -------------------------------
+    // Espera para depuración con GDB
+    volatile int wait_for_gdb = 1;
+    while(wait_for_gdb);  // cambia a 0 desde GDB para continuar
+    // -------------------------------
 
-	// Initialize the EGL display connection
-	eglInitialize(mWindow, nullptr, nullptr);
+    // Connect to the EGL default display
+    mWindow = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    if (!mWindow)
+        return;
 
-	// Select OpenGL ES as the desired graphics API
-	if (eglBindAPI(EGL_OPENGL_ES_API) == EGL_FALSE)
-	{
-		eglTerminate(mWindow);
-		return;
-	}
+    // Initialize the EGL display connection
+    eglInitialize(mWindow, nullptr, nullptr);
 
-	// Get an appropriate EGL framebuffer configuration
-	EGLConfig config;
-	EGLint numConfigs;
-	static const EGLint framebufferAttributeList[] =
-	{
-		EGL_RED_SIZE,     8,
-		EGL_GREEN_SIZE,   8,
-		EGL_BLUE_SIZE,    8,
-		EGL_ALPHA_SIZE,   8,
-		EGL_DEPTH_SIZE,   24,
-		EGL_STENCIL_SIZE, 8,
-		EGL_NONE
-	};
-	eglChooseConfig(mWindow, framebufferAttributeList, &config, 1, &numConfigs);
-	if (numConfigs == 0)
-	{
-		eglTerminate(mWindow);
-		return;
-	}
+    // Select OpenGL ES as the desired graphics API
+    if (eglBindAPI(EGL_OPENGL_ES_API) == EGL_FALSE)
+    {
+        eglTerminate(mWindow);
+        return;
+    }
 
-	// Create an EGL window surface
-	mSurface = eglCreateWindowSurface(mWindow, config, nwindowGetDefault(), nullptr);
-	if (!mSurface)
-	{
-		eglTerminate(mWindow);
-		return;
-	}
+    // Get an appropriate EGL framebuffer configuration
+    EGLConfig config;
+    EGLint numConfigs;
+    static const EGLint framebufferAttributeList[] =
+    {
+        EGL_RED_SIZE,     8,
+        EGL_GREEN_SIZE,   8,
+        EGL_BLUE_SIZE,    8,
+        EGL_ALPHA_SIZE,   8,
+        EGL_DEPTH_SIZE,   24,
+        EGL_STENCIL_SIZE, 8,
+        EGL_NONE
+    };
+    eglChooseConfig(mWindow, framebufferAttributeList, &config, 1, &numConfigs);
+    if (numConfigs == 0)
+    {
+        eglTerminate(mWindow);
+        return;
+    }
 
-	// Create an EGL rendering context (OpenGL ES 2.0)
-	static const EGLint contextAttributeList[] =
-	{
-		EGL_CONTEXT_CLIENT_VERSION, 2,
-		EGL_NONE
-	};
-	mContext = eglCreateContext(mWindow, config, EGL_NO_CONTEXT, contextAttributeList);
-	if (!mContext)
-	{
-		eglDestroySurface(mWindow, mSurface);
-		eglTerminate(mWindow);
-		return;
-	}
+    // Create an EGL window surface
+    mSurface = eglCreateWindowSurface(mWindow, config, nwindowGetDefault(), nullptr);
+    if (!mSurface)
+    {
+        eglTerminate(mWindow);
+        return;
+    }
 
-	// Connect the context to the surface
-	eglMakeCurrent(mWindow, mSurface, mSurface, mContext);
+    // Create an EGL rendering context (OpenGL ES 2.0)
+    static const EGLint contextAttributeList[] =
+    {
+        EGL_CONTEXT_CLIENT_VERSION, 2,
+        EGL_NONE
+    };
+    mContext = eglCreateContext(mWindow, config, EGL_NO_CONTEXT, contextAttributeList);
+    if (!mContext)
+    {
+        eglDestroySurface(mWindow, mSurface);
+        eglTerminate(mWindow);
+        return;
+    }
 
-	eglSwapInterval(mWindow, 1);
+    // Connect the context to the surface
+    eglMakeCurrent(mWindow, mSurface, mSurface, mContext);
 
-	if (mGLInterface == nullptr)
-	{
-		mGLInterface = new GLInterface(this);
-		if (!InitGLInterface())
-		{
-			delete mGLInterface;
-			mGLInterface = nullptr;
-			return;
-		}
+    eglSwapInterval(mWindow, 1);
 
-		mGLInterface->UpdateViewport();
-		mWidgetManager->Resize(mScreenBounds, mGLInterface->mInputSourceRect);
-	}
+    if (mGLInterface == nullptr)
+    {
+        mGLInterface = new GLInterface(this);
+        if (!InitGLInterface())
+        {
+            delete mGLInterface;
+            mGLInterface = nullptr;
+            return;
+        }
 
-	bool isActive = mActive;
-	mActive = true;
+        mGLInterface->UpdateViewport();
+        mWidgetManager->Resize(mScreenBounds, mGLInterface->mInputSourceRect);
+    }
 
-	mPhysMinimized = false;
+    bool isActive = mActive;
+    mActive = true;
 
-	if (isActive != mActive)
-		RehupFocus();
+    mPhysMinimized = false;
 
-	ReInitImages();
+    if (isActive != mActive)
+        RehupFocus();
 
-	mWidgetManager->mImage = mGLInterface->GetScreenImage();
-	mWidgetManager->MarkAllDirty();
+    ReInitImages();
+
+    mWidgetManager->mImage = mGLInterface->GetScreenImage();
+    mWidgetManager->MarkAllDirty();
 }
