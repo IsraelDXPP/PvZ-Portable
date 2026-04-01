a306ea762da5f527280a53f7218f6dbc5722ddf7
Zhou Qiankang <wszqkzqk@qq.com>
2026-04-01 15:39:14 +0800

    Fix: update OpenGL ES initialization for Nintendo Switch
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

--- src/SexyAppFramework/graphics/GLPlatform.h
+++ src/SexyAppFramework/graphics/GLPlatform.h
@@ -36,6 +36,14 @@
 #pragma warning(pop)
 #endif
 
+#include <SDL.h>
+
+#ifdef __SWITCH__
+#include <switch.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#endif
+
 // Shared macro definitions — identical keywords in GLSL ES 1.00 and GLSL 1.20
 #define GLSL_VERT_MACROS \
 	"#define VERT_IN attribute\n" \
@@ -49,26 +57,13 @@
 // When true, a desktop GL compatibility context is in use and shaders
 extern bool gDesktopGLFallback;
 
-#ifdef NINTENDO_SWITCH
-
-#include <switch.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-
 inline void PlatformGLInit()
 {
+#ifdef __SWITCH__
 	gladLoadGLES2((GLADloadfunc)eglGetProcAddress);
-}
-
 #else
-
-#include <SDL.h>
-
-inline void PlatformGLInit()
-{
 	gladLoadGLES2((GLADloadfunc)SDL_GL_GetProcAddress);
-}
-
 #endif
+}
 
 #endif // __GLPLATFORM_H__
